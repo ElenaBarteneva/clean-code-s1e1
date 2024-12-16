402b20622f7371db0e16ce93f0e60a29f877a15c
unknown <lena.kilian91@gmail.com>
2024-12-17 00:27:19 +0800

    refactor: advanced 1.1 adding semantic tags

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ var completedTasksHolder=document.getElementById("done-tasks");//completed-tasks
 //New task list item
 var createNewTaskElement=function(taskString){
 
-    var listItem=document.createElement("li");
+    var listItem=document.createElement("article");
 
     //input (checkbox)
-    var checkBox=document.createElement("input");//checkbx
+    var checkBox=document.createElement("input");//checkbox
     //label
     var label=document.createElement("label");//label
     //input (text)
