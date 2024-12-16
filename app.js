ef6b8b7aeae767dfb149ed789ea5bcbd9ed31f3b
unknown <lena.kilian91@gmail.com>
2024-12-17 01:09:18 +0800

    refactor: 1.3 changing to double quotes

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task name';
+    label.className="task name";
 
     //Each elements, needs appending
     checkBox.type="checkbox";
@@ -83,7 +83,7 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('input[type=text]');
+    var editInput=listItem.querySelector("input[type=text]");
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
     var containsClass=listItem.classList.contains("edit-mode");
